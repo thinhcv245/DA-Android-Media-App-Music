15abeebc1a186f4a21092d85a3d0eb8090af1ff8
CVT\Chau Thinh <thinh.chauvan2405@gmail.com>
2023-03-03 17:07:08 +0700

    init DA Media App Music

---
+++ MediaAppMusic/app/src/main/java/com/example/mediaappmusic/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mediaappmusic;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, PersonalActivity.class);
+                startActivity(i);
+            }
+        }, 3000);
+    }
+}
