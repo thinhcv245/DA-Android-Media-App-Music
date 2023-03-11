0a2e9b3d08cd851caec3d5a5d22214f37e6fa4bd
CVT\Chau Thinh <thinh.chauvan2405@gmail.com>
2023-03-11 11:01:47 +0700

    update

--- MediaAppMusic/app/src/main/java/com/example/mediaappmusic/PlayMusicActivity.java
+++ MediaAppMusic/app/src/main/java/com/example/mediaappmusic/PlayMusicActivity.java
@@ -10,16 +10,19 @@ import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
-import java.time.Duration;
 
 public class PlayMusicActivity extends AppCompatActivity {
 
@@ -31,7 +34,11 @@ public class PlayMusicActivity extends AppCompatActivity {
 
     //String url = "https://cdn.pixabay.com/download/audio/2021/12/16/audio_232a4bdedf.mp3";
     //String url = "https://dl179.dlmate15.online/?file=M3R4SUNiN3JsOHJ6WWQ2a3NQS1Y5ZGlxVlZIOCtyZ011ZXh4NjFVQVV1NFU5c1liNWFHRGQ0d1hmSWtvaHFUbk1vMFJyRzN4T3B1dkVUdWlsN2x0V2ppeDVkazU4Zzc3dUt3RmVQMUxIVG5ldmMrQXZ4QmF3aXJBVGZIdVhvNS9TVkpVN1UxemdHQ1Z4ZjNSaGdYNXAzWHc0aCtsZFNzZnBpNEpPTENDMTVKWWhpeVpTYStoZ2JZc2x3Rzh4NTBaOWQ2U3QwLzcwK0Y0djlsNkEzSmFOWlZGeVpQbjJ1VEZxRUpFMGM5TGdoLzMrckwxVnRnbkdhT2hlajkxUFNsVXVLbjhWUUVia25CTnFEam5yLzlqL1haVWRhSWo%3D";
-    String url = "https://dl.sndup.net/s9p8/haylatrangnoi.mp3";
+    //String url = "https://dl.sndup.net/s9p8/haylatrangnoi.mp3";
+    //String url = "https://dl.sndup.net/xv7t/Hay-La-Trang-Noi.mp3";
+    //String url = "https://dl.sndup.net/xv7t/Hay-La-Trang-Noi.mp3";
+    //String url = "https://dl.sndup.net/rpp5/Cuoi-Cung-Thi.mp3";
+    String url = "https://dl.sndup.net/k2pp/Khuat-Loi_H-Kray.mp3";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,7 +70,7 @@ public class PlayMusicActivity extends AppCompatActivity {
                 System.out.println("release");
                 //mp.release();
                 //pauseSong();
-                createMediaPlayer();
+                //createMediaPlayer();
             }
         });
 
@@ -88,15 +95,9 @@ public class PlayMusicActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(mediaPlayer.isPlaying()) {
-//                    imageViewPlayOrPause.setImageResource(R.drawable.icon_pause);
-//                    imageViewRotate.clearAnimation();
-//                    mediaPlayer.pause();
                     pauseSong();
                 }
                 else {
-//                    imageViewPlayOrPause.setImageResource(R.drawable.icon_play);
-//                    imageViewRotate.startAnimation(rotateAnimation);
-//                    mediaPlayer.start();
                     playSong();
                 }
             }
@@ -136,6 +137,9 @@ public class PlayMusicActivity extends AppCompatActivity {
                 SimpleDateFormat hourseFormat = new SimpleDateFormat("mm:ss");
                 textViewStartTime.setText(hourseFormat.format(mediaPlayer.getCurrentPosition()));
                 seekBarSong.setProgress(mediaPlayer.getCurrentPosition());
+                if(seekBarSong.getMax() == seekBarSong.getProgress()) {
+                    createMediaPlayer();
+                }
                 handler.postDelayed(this, 500);
             }
         },500);
@@ -150,4 +154,23 @@ public class PlayMusicActivity extends AppCompatActivity {
         imageViewRotate.clearAnimation();
         mediaPlayer.pause();
     }
+
+//    public String loadJSONFromAsset() {
+//        String json = null;
+//        try {
+//            System.out.println("vô");
+//            InputStream is = getAssets().open("data.json");
+//            System.out.println("thành công");
+//            int size = is.available();
+//            byte[] buffer = new byte[size];
+//            is.read(buffer);
+//            is.close();
+//            System.out.println("close");
+//            json = new String(buffer, "UTF-8");
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//            return null;
+//        }
+//        return json;
+//    }
 }
