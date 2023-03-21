9b463b6cca36481fdac36992e639535b059e127a
CVT\Chau Thinh <thinh.chauvan2405@gmail.com>
2023-03-21 16:00:27 +0700

    complete View Activity Top 100

--- MediaAppMusic/app/src/main/java/com/example/mediaappmusic/Top100Activity.java
+++ MediaAppMusic/app/src/main/java/com/example/mediaappmusic/Top100Activity.java
@@ -26,48 +26,37 @@ import retrofit2.Response;
 
 public class Top100Activity extends AppCompatActivity {
 
-    RecyclerView recyclerView0, recyclerView1, recyclerView2, recyclerView3, recyclerView4, recyclerView5;
-    TextView textViewTitle0, textViewTitle1, textViewTitle2, textViewTitle3, textViewTitle4, textViewTitle5;
     ImageView imageViewBanner;
-
-//    ArrayList<PlayList> playLists;
+    ArrayList<RecyclerView> recyclerViews;
+    ArrayList<TextView> textViews;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top100);
 
+        recyclerViews   = new ArrayList<>();
+        textViews       = new ArrayList<>();
         imageViewBanner         = findViewById(R.id.top100_imageView_banner);
-        recyclerView0           = findViewById(R.id.top100_recyclerView_recycle0);
-        recyclerView1           = findViewById(R.id.top100_recyclerView_recycle1);
-        recyclerView2           = findViewById(R.id.top100_recyclerView_recycle2);
-        recyclerView3           = findViewById(R.id.top100_recyclerView_recycle3);
-        recyclerView4           = findViewById(R.id.top100_recyclerView_recycle4);
-        recyclerView5           = findViewById(R.id.top100_recyclerView_recycle5);
-        textViewTitle0          = findViewById(R.id.top100_textView_title0);
-        textViewTitle1          = findViewById(R.id.top100_textView_title1);
-        textViewTitle2          = findViewById(R.id.top100_textView_title2);
-        textViewTitle3          = findViewById(R.id.top100_textView_title3);
-        textViewTitle4          = findViewById(R.id.top100_textView_title4);
-        textViewTitle5          = findViewById(R.id.top100_textView_title5);
-        getResources().getIdentifier()
 
-        imageViewBanner.setImageDrawable(Utilities.LoadImageFromAssets(Top100Activity.this, "banner.jpg"));
+        for(int i = 0; i < 6; i++) {
+            try{
+                int id = getResources().getIdentifier("top100_recyclerView_recycle" + i, "id", this.getPackageName());
+                recyclerViews.add(findViewById(id));
+                int idText = getResources().getIdentifier("top100_textView_title" + i, "id",this.getPackageName());
+                textViews.add(findViewById(idText));
+            }
+            catch (Exception e) {
+                System.out.println("Error " + i);
+            }
+        }
+
+        Drawable drawable = Utilities.LoadImageFromAssets(Top100Activity.this, "banner.jpg");
+        if(drawable != null) {
+            imageViewBanner.setImageDrawable(drawable);
+        }
 
 
         CallPlayListTop100();
-//        if(playLists != null) {
-//            System.out.println(playLists.size());
-//        }
-//        else {
-//            System.out.println("null");
-//        }
-
-
-//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-//        albums = new ArrayList<>();
-//        AlbumAdapter albumAdapter = new AlbumAdapter(this, albums);
-//        recyclerViewNoiBat.setAdapter(albumAdapter);
-//        recyclerViewNoiBat.setLayoutManager(linearLayoutManager);
     }
     public void CallPlayListTop100() {
         IApiService.iApiService.getPlayListTop100().enqueue(new Callback<ArrayList<PlayList>>() {
@@ -75,57 +64,22 @@ public class Top100Activity extends AppCompatActivity {
             public void onResponse(Call<ArrayList<PlayList>> call, Response<ArrayList<PlayList>> response) {
                 if (response.isSuccessful()) {
                     ArrayList<PlayList> playLists = response.body();
-                    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(Top100Activity.this, LinearLayoutManager.HORIZONTAL, false);
 
-                    ArrayList<Album> albums0 = playLists.get(0).getItems();
-                    AlbumAdapter albumAdapter0 = new AlbumAdapter(Top100Activity.this, albums0);
-                    recyclerView0.setAdapter(albumAdapter0);
-                    recyclerView0.setLayoutManager(linearLayoutManager);
-
-                    ArrayList<Album> albums1 = playLists.get(1).getItems();
-                    AlbumAdapter albumAdapter1 = new AlbumAdapter(Top100Activity.this, albums1);
-                    recyclerView1.setAdapter(albumAdapter1);
-                    recyclerView1.setLayoutManager(new LinearLayoutManager(Top100Activity.this, LinearLayoutManager.HORIZONTAL, false));
-
-//                    ArrayList<Album> albums2 = playLists.get(2).getItems();
-//                    AlbumAdapter albumAdapter2 = new AlbumAdapter(Top100Activity.this, albums2);
-//                    recyclerView2.setAdapter(albumAdapter2);
-//                    recyclerView2.setLayoutManager(linearLayoutManager);
-//
-//                    ArrayList<Album> albums3 = playLists.get(3).getItems();
-//                    AlbumAdapter albumAdapter3 = new AlbumAdapter(Top100Activity.this, albums3);
-//                    recyclerView3.setAdapter(albumAdapter3);
-//                    recyclerView3.setLayoutManager(linearLayoutManager);
-
-//                    ArrayList<Album> albums4 = playLists.get(4).getItems();
-//                    AlbumAdapter albumAdapter4 = new AlbumAdapter(Top100Activity.this, albums4);
-//                    recyclerView0.setAdapter(albumAdapter4);
-//                    recyclerView0.setLayoutManager(linearLayoutManager);
-//
-//                    ArrayList<Album> albums5 = playLists.get(5).getItems();
-//                    AlbumAdapter albumAdapter5 = new AlbumAdapter(Top100Activity.this, albums5);
-//                    recyclerView0.setAdapter(albumAdapter5);
-//                    recyclerView0.setLayoutManager(linearLayoutManager);
-
-
-//                    for (PlayList item:playLists) {
-//                        System.out.println(item.getTitle().compareToIgnoreCase("Nổi bật") == 0);
-//                        if(item.getTitle().compareToIgnoreCase("Nổi bật") == 0) {
-//                            albums = item.getItems();
-//                            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(Top100Activity.this, LinearLayoutManager.HORIZONTAL, false);
-//                            AlbumAdapter albumAdapter = new AlbumAdapter(Top100Activity.this, albums);
-//                            recyclerViewNoiBat.setAdapter(albumAdapter);
-//                            recyclerViewNoiBat.setLayoutManager(linearLayoutManager);
-//                            return;
-//                        }
-//                        System.out.println(item.getTitle());
-//                    }
+                    for(int i = 0; i < playLists.size(); i++) {
+                        AlbumAdapter albumAdapter = new AlbumAdapter(Top100Activity.this, playLists.get(i).getItems());
+                        RecyclerView recyclerView = recyclerViews.get(i);
+                        if(recyclerView != null) {
+                            recyclerView.setAdapter(albumAdapter);
+                            recyclerView.setLayoutManager(new LinearLayoutManager(Top100Activity.this, LinearLayoutManager.HORIZONTAL, false));
+                        }
+                        TextView textView = textViews.get(i);
+                        textView.setText(playLists.get(i).getTitle());
+                    }
                 }
             }
             @Override
             public void onFailure(Call<ArrayList<PlayList>> call, Throwable t) {
                 Toast.makeText(Top100Activity.this, "Call API Error", Toast.LENGTH_LONG).show();
-                System.out.println("Error");
             }
         });
     }
